ea92e77caa71e53fb98cb3e2469162c248c65b90
femaledream34 <95567447+femaledream34@users.noreply.github.com>
2022-05-07 14:14:04 +0530

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,33 @@
+body{
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    background-color: blanchedalmond;
+}
+.header_div{
+    background: whitesmoke;
+    color:blueviolet;
+}
+.header_div p{
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    color:bisque;
+    letter-spacing:2px;
+    font: size 12px;
+    font-weight: bold;
+}
+.main_div button{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    margin-top:30px;
+    width:130px;
+    font-weight:bolder;
+    letter-spacing:3px;
+}
+.copy_right_footer{
+    position:fixed;
+    color:aquamarine;
+    bottom:0;
+    width:100%;
+    font-size:18px;
+    background-color: aqua;
+    line-height:3px;
+    text-align:center;
+    text-shadow:5px 5px 5px wheat;
+}
